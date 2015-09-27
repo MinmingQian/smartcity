748c69ef644e41e4887770e24c6df9c7b5be57b4
MinmingQian <MinmingQian@users.noreply.github.com>
2015-09-27 15:48:23 +0930

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'smartcity');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'pGRlHKvEId');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
